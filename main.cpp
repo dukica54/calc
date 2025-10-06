7e563b1f1300aa94c6deac35541c72a6df70ec4a
Matija Dukaric <matijadukaric1@gmail.com>
2025-10-06 21:06:16 +0200

    Prva sprememba

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+
+int main() {
+    double num1, num2;
+    char op;
+
+    cout << "Vnesi problem (5 + 3): ";
+    cin >> num1 >> op >> num2;
+
+    double result;
+
+    if (op == '+') {
+        result = num1 + num2;
+    } else if (op == '-') {
+        result = num1 - num2;
+    } else if (op == '*') {
+        result = num1 * num2;
+    } else if (op == '/') {
+        if (num2 != 0) {
+            result = num1 / num2;
+        } else {
+            cout << "Napaka! Deljenje z 0." << endl;
+            return 1;
+        }
+    } else {
+        cout << "Napaka! Neznan operator." << endl;
+        return 1;
+    }
+
+    cout << "Rezultat: " << result << endl;
+    return 0;
+}
